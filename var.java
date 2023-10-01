ac83c61ce93db254f278e85d57ebdc5108ac13cc
Rono Beatrice <ronobc24@gmail.com>
2023-10-01 19:19:24 +0300

    This demonstrates how to declare and use String variables in Java.

---
+++ var.java
@@ -0,0 +1,19 @@
+//Variables are containers for storing data values.
+
+// In Java, there are different types of variables, for example:
+
+// String - stores text, such as "Hello". String values are surrounded by double quotes
+// int - stores integers (whole numbers), without decimals, such as 123 or -123
+// float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+// char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// boolean - stores values with two states: true or false
+
+// Declaring (Creating) Variables -- To create a variable, you must specify the type and assign it a value: type variableName = value;
+// Where type is one of Java's types (such as int or String), and variableName is the name of the variable (such as x or name). The equal sign is used to assign values to the variable.
+
+public class vaR{
+    public static void main(String[] args){
+        String surname = "Rono";
+        System.out.println(surname)
+    }
+}

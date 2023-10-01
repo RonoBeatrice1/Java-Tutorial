6f26e09cc84fb2536fdfadb5925f1f1be82a4225
Rono Beatrice <ronobc24@gmail.com>
2023-10-01 18:40:32 +0300

    Created a simple Java program that prints "Hello World" to the console. This is a basic example to get started with Java programming.

---
+++ Main.java
@@ -0,0 +1,9 @@
+//In Java, an object is created from a class
+// Every line of code that runs in Java must be inside a class. --  A class should always start with an uppercase first letter. 
+//System is a built-in Java class that contains useful members, such as out, which is short for "output". The println() method, short for "print line", is used to print a value to the screen (or a file).
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
